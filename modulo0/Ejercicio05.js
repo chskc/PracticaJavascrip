7adaf0295e06914e55055794c2cb968214b5c90a
Francheska <frankeka.jose@gmail.com>
2024-02-09 23:40:09 +0100

    Ejercicio05

---
+++ modulo0/Ejercicio05.js
@@ -0,0 +1,10 @@
+//Declara 3 variables con los nombres y valores siguientes
+//``firstName = 'Jon'; lastName = 'Snow'; age = 24``;
+//Muestralos por consola de esta forma: Soy Jon Snow, Tengo 24 años y me gustan los lobos.
+
+const firstName = "Juan";
+const lastName = "Nieves";
+const age = 24;
+
+//para los espacios vacios debo poner las comillas ""
+console.log("Soy "+firstName+" "+lastName+", Tengo "+age+" años y me gustan los lobos");
