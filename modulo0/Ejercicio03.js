422bbd54a7d9105838778fee2c12e36067f11844
Francheska <frankeka.jose@gmail.com>
2024-02-09 23:00:39 +0100

    Ejercicio3

---
+++ modulo0/Ejercicio03.js
@@ -0,0 +1,9 @@
+//Crea una variable llamada x con el valor 5 y otra y con el valor 10.
+//Crea una tercera variable z y asignale el valor de x + y.
+
+const x = 5;
+const y = 10;
+
+let z = x + y;
+
+console.log(z);
