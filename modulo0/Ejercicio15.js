80155876b4796f5632de1c4327e543ea0d6f4a05
Francheska <frankeka.jose@gmail.com>
2024-02-13 22:49:07 +0100

    Ejercicio16

--- modulo0/Ejercicio15.js
+++
@@ -1,8 +0,0 @@
-//Alerta el numero de elementos en el array usando la propiedad correcta de Array.
-
-
-const cars = ["Saab", "Volvo", "BMW"];
-
-alert(cars.length);
-
-//length sirve para contar cuantos elementos tengo dentro
---
+++ modulo0/Ejercicio16.js
@@ -0,0 +1,11 @@
+//Añade 2 elementos al array: "Morty" y "Summer".
+//Muestra en consola el último personaje del array
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+rickAndMortyCharacters.push("Morty","Summer");
+
+let ultimo = rickAndMortyCharacters.length - 1;
+
+
+console.log(rickAndMortyCharacters[ultimo]);
