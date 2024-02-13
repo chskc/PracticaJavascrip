c403ae47e5f997686670c9284507b72103e50f9a
Francheska <frankeka.jose@gmail.com>
2024-02-13 22:40:50 +0100

    Ejercicio15

---
+++ modulo0/Ejercicio15.js
@@ -0,0 +1,8 @@
+//Alerta el numero de elementos en el array usando la propiedad correcta de Array.
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+alert(cars.length);
+
+//length sirve para contar cuantos elementos tengo dentro
