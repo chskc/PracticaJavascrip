9af39054e16f6e11e26300e51327f62feaed2692
Francheska <frankeka.jose@gmail.com>
2024-02-13 23:01:14 +0100

    Ejercicio18

--- modulo0/Ejercicio17.js
+++
@@ -1,9 +0,0 @@
-//Elimina el último elemento del array y muestra el primero y el último por consola.
-
-
-const RickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
-
-RickAndMortyCharacters.pop();
-//para eliminar el ultimo elemento
-
-console.log(RickAndMortyCharacters[0]+" "+RickAndMortyCharacters[RickAndMortyCharacters.length-1]);
