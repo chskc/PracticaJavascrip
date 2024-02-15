feede7d39739a445b09d94fc8b000fdbf2b966b4
Francheska <frankeka.jose@gmail.com>
2024-02-15 23:02:50 +0100

    Ejercicio21

---
+++ modulo0/Ejercicio21.js
@@ -0,0 +1,19 @@
+//Crea un bucle for que vaya desde 0 a 9 y
+//muestralo por consola solo cuando el resto del numero dividido entre 2 sea 0.
+
+/*
+
+if
+switch */
+
+
+for(let numero = 0 ; numero<=9 ; numero++){
+
+    if((numero%2) == 0){
+    
+    console.log(numero);
+    
+    }
+    
+    
+    }
